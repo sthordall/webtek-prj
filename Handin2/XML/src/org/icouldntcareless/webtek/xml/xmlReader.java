7aaa2b4f223fef5276a702b0a103ab9ba0e65c42
Shiphter <stephantl@gmail.com>
2014-02-09 21:21:53 +0100

    Added functionality to generateItemFromDoc

--- Handin2/XML/src/org/icouldntcareless/webtek/xml/xmlReader.java
+++ Handin2/XML/src/org/icouldntcareless/webtek/xml/xmlReader.java
@@ -1,19 +1,38 @@
 package org.icouldntcareless.webtek.xml;
 
 import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.Namespace;
 
 public class xmlReader {
-
+	private static String WebTekNameSpace = "http://www.cs.au.dk/dWebTek/2014";
+	private static final Namespace WEBTEKNAMESPACE = Namespace.getNamespace(WebTekNameSpace);
+	
 	public static void main(String[] args) {
 
 		// Generate JDOM Document from given file
 
-		// Get information from document
+	}
 
+	public Item generateItemFromDoc(Document itemDocument){
+		// Get information from document
+		Element rootElement = itemDocument.getRootElement();
+		Element itemElement = rootElement.getChild("item", WEBTEKNAMESPACE);
+		
+		Item item = new Item(itemElement.getAttributeValue("itemID", WEBTEKNAMESPACE),
+								itemElement.getAttributeValue("itemName", WEBTEKNAMESPACE), 
+								itemElement.getAttributeValue("itemURL", WEBTEKNAMESPACE), 
+								itemElement.getAttributeValue("itemPrice", WEBTEKNAMESPACE), 
+								itemElement.getAttributeValue("itemStock", WEBTEKNAMESPACE), 
+								itemElement.getAttributeValue("itemDescription", WEBTEKNAMESPACE));
+		
+		
+		return item;
 	}
 
 	public Document generateCreateItemDoc(Item item) {
 		// Generate "CreateItem" XML request, with initial item data
+
 		return null;
 	}
 
