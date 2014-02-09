fe5cfab40eccdcea359f8c031b5001fdf1800982
Shiphter <stephantl@gmail.com>
2014-02-09 20:41:24 +0100

    Possible required functions added

--- Handin2/XML/src/org/icouldntcareless/webtek/xml/xmlReader.java
+++ Handin2/XML/src/org/icouldntcareless/webtek/xml/xmlReader.java
@@ -1,5 +1,39 @@
 package org.icouldntcareless.webtek.xml;
 
+import org.jdom2.Document;
+
 public class xmlReader {
+
+	public static void main(String[] args) {
+
+		// Generate JDOM Document from given file
+
+		// Get information from document
+
+	}
+
+	public Document generateCreateItemDoc(Item item) {
+		// Generate "CreateItem" XML request, with initial item data
+		return null;
+	}
+
+	public String httpPostCreateItem(Document createItemDoc) {
+		// HTTP POST request "CreateItem" to cloud server
+
+		// Get response from cloud server
+		return null;
+	}
+
+	public Document generateModifyItemDoc(Item item) {
+		// Generate "ModifyItem" XML request, with rest of item data
+		return null;
+	}
+
+	public String httpPostModifyItem(Document modifyItemDoc,
+			String serverResponse) {
+		// /Use response from cloud server, to send "ModifyItem"
+
+		return null;
+	}
 
 }
