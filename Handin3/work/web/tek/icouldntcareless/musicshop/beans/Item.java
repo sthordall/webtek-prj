bd63d27ea373955e3457d6c4153a2fa9b014d297
Shiphter <stephantl@gmail.com>
2014-02-16 15:39:46 +0100

    Adding datamodel representation of item.

---
+++ Handin3/work/web/tek/icouldntcareless/musicshop/beans/Item.java
@@ -0,0 +1,73 @@
+package web.tek.icouldntcareless.musicshop.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class Item implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String itemID;
+	
+	private String itemName;
+
+	private String itemURL;
+	
+	private String itemPrice;
+	
+	private String itemStock;
+	
+	private String itemDescription;
+
+	public String getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(String itemID) {
+		this.itemID = itemID;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemURL() {
+		return itemURL;
+	}
+
+	public void setItemURL(String itemURL) {
+		this.itemURL = itemURL;
+	}
+
+	public String getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(String itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	public String getItemStock() {
+		return itemStock;
+	}
+
+	public void setItemStock(String itemStock) {
+		this.itemStock = itemStock;
+	}
+
+	public String getItemDescription() {
+		return itemDescription;
+	}
+
+	public void setItemDescription(String itemDescription) {
+		this.itemDescription = itemDescription;
+	}
+	
+}
