b991f7696ed831851b9cba47f546df13e28f8ecf
Shiphter <stephantl@gmail.com>
2014-02-18 21:51:14 +0100

    Added getCreateItemRequest and getModifyItemRequest from XMLReader in Handin2

--- Handin3/work/web/tek/icouldntcareless/musicshop/helpers/XMLParser.java
+++ Handin3/work/web/tek/icouldntcareless/musicshop/helpers/XMLParser.java
@@ -2,10 +2,12 @@ package web.tek.icouldntcareless.musicshop.helpers;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.input.sax.XMLReaderJDOMFactory;
 import org.jdom2.input.sax.XMLReaderXSDFactory;
@@ -20,4 +22,44 @@ public class XMLParser {
 		return document.getRootElement();
 
 	}
+	
+	public Document getCreateItemRequest(Element item, String shopKey,
+			Namespace namespace) {
+		Element createItem = new Element("createItem");
+		createItem.addNamespaceDeclaration(namespace);
+
+		createItem.setNamespace(namespace);
+		createItem.addContent(new Element("shopKey").setText(shopKey)
+				.setNamespace(namespace));
+		createItem.addContent(item.getChild("itemName", namespace).clone());
+
+		return new Document(createItem);
+	}
+	
+	public Document getModifyItemRequest(Element item, Element itemID,
+			String shopKey, Namespace namespace) {
+
+		Element modifyItem = new Element("modifyItem");
+		modifyItem.addNamespaceDeclaration(namespace);
+		modifyItem.setNamespace(namespace);
+
+		modifyItem.addContent(new Element("shopKey").setText(shopKey)
+				.setNamespace(namespace));
+
+		List<Element> itemChildren = item.getChildren();
+		for (Element element : itemChildren) {
+			if (element.getName() == "itemID") {
+				modifyItem.addContent(new Element("itemID").setText(itemID
+						.getText()).setNamespace(namespace));
+			} else if (element.getName() == "itemStock") {
+				// DO NOTHING
+			} else {
+				modifyItem.addContent(element.clone());
+			}
+		}
+		
+		return new Document(modifyItem);
+
+	}
+	
 }
