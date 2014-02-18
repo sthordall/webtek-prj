498d0cbf06b73d9370b16186b1038d8ceef9f5e7
Shiphter <stephantl@gmail.com>
2014-02-18 21:53:35 +0100

    Initial work on Items, and requesting from cloud server. Still not working.

--- Handin3/work/web/tek/icouldntcareless/musicshop/beans/Items.java
+++ Handin3/work/web/tek/icouldntcareless/musicshop/beans/Items.java
@@ -1,28 +1,42 @@
 package web.tek.icouldntcareless.musicshop.beans;
 
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import web.tek.icouldntcareless.musicshop.helpers.HttpHandler;
+import web.tek.icouldntcareless.musicshop.helpers.XMLParser;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @ManagedBean(name = "Items", eager = true)
-@ApplicationScoped
+@RequestScoped
 public class Items implements Serializable {
+
+	private HttpHandler httpHandler;
+	private XMLParser xmlParser;
 	
 	private static final long serialVersionUID = 2597636401051332599L;
-
+	
 	private ArrayList<Item> itemList;
 	
 	@PostConstruct
 	public void init() {
+		httpHandler = new HttpHandler();
+		xmlParser = new XMLParser();
+		
+//		URL requestUrl = new URL(ApplicationConstants.CLOUDURL+ApplicationConstants.LISTITEMS);
+			
+//		xmlParser.getRootElement(httpHandler.outputXMLonHTTP("GET", requestUrl, docToOutput), schema)
+		
+//		itemList = new ArrayList<>();
 		itemList = new ArrayList<Item>(Arrays.asList(
 				new Item("8", "Gibson", "www.guitar.dk", "1495", "3", "Very Nice!"),
-				new Item("110", "Bummelum", "www.Trommer.dk", "14995", "8", "Very Super Nice!")));
+				new Item("120", "Bummelum", "www.Trommer.dk", "14995", "8", "Very Super Nice!")));
 		
 	}
-	
 	
 	public ArrayList<Item> getItemList() {
 		return itemList;
