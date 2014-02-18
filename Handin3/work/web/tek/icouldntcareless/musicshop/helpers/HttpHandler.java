9f84ee76dd29a472ba0a42f9379ba73db2527140
Shiphter <stephantl@gmail.com>
2014-02-18 21:45:14 +0100

    Comments on existing functions, and new function for requesting with no xml. To be used for listItems request.

--- Handin3/work/web/tek/icouldntcareless/musicshop/helpers/HttpHandler.java
+++ Handin3/work/web/tek/icouldntcareless/musicshop/helpers/HttpHandler.java
@@ -11,8 +11,26 @@ import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.XMLOutputter;
 
+/**
+ * @author shiphter
+ * Handles all http requests
+ */
 public class HttpHandler {
 	
+	
+	
+	/**
+	 * Outputs XML JDOM Document on given URL.
+	 * Returns response as JDOM Document, if no response, NULL is returned.
+	 * 
+	 * @param httpRequestType
+	 * @param url
+	 * @param docToOutput
+	 * @return Document
+	 * @throws ProtocolException
+	 * @throws IOException
+	 * @throws JDOMException
+	 */
 	public Document outputXMLonHTTP(String httpRequestType, URL url,
 			Document docToOutput) throws ProtocolException, IOException,
 			JDOMException {
@@ -45,5 +63,39 @@ public class HttpHandler {
 		return responseDocument;
 	}
 	
-	
+	/**
+	 * Request on URL parameter, and returns response as a JDOM Document.
+	 * If no response, return NULL.
+	 * 
+	 * @param httpRequestType
+	 * @param url
+	 * @return
+	 * @throws IOException
+	 */
+	public Document HttpRequest(String httpRequestType, URL url) throws IOException {
+		
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod(httpRequestType);
+		con.setDoOutput(true);
+		con.setDoInput(true);
+		con.connect();
+
+		Document responseDocument = null;
+		
+		try {
+			SAXBuilder builder = new SAXBuilder();
+			responseDocument = builder.build(con.getInputStream());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		if (con.getResponseCode() != 200) {
+			System.out.print("An network error occurred: "
+					+ con.getResponseCode() + " - " + con.getResponseMessage());
+		}
+
+		con.disconnect();
+
+		return responseDocument;
+	}
 }
