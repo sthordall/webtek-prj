9b419a19fef76539cf829b10212bc9850a4468d9
Shiphter <stephantl@gmail.com>
2014-02-18 17:01:04 +0100

    Hardcoded items, and changed list to ArrayList

--- Handin3/work/web/tek/icouldntcareless/musicshop/beans/Items.java
+++ Handin3/work/web/tek/icouldntcareless/musicshop/beans/Items.java
@@ -1,24 +1,34 @@
 package web.tek.icouldntcareless.musicshop.beans;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-
 import java.io.Serializable;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-@ManagedBean
-@SessionScoped
+@ManagedBean(name = "Items", eager = true)
+@ApplicationScoped
 public class Items implements Serializable {
 	
 	private static final long serialVersionUID = 2597636401051332599L;
 
-	private List<Item> itemList;
-
-	public List<Item> getItemList() {
+	private ArrayList<Item> itemList;
+	
+	@PostConstruct
+	public void init() {
+		itemList = new ArrayList<Item>(Arrays.asList(
+				new Item("8", "Gibson", "www.guitar.dk", "1495", "3", "Very Nice!"),
+				new Item("110", "Bummelum", "www.Trommer.dk", "14995", "8", "Very Super Nice!")));
+		
+	}
+	
+	
+	public ArrayList<Item> getItemList() {
 		return itemList;
 	}
 
-	public void setItemList(List<Item> itemList) {
+	public void setItemList(ArrayList<Item> itemList) {
 		this.itemList = itemList;
 	}
 	
