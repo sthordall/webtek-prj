a15c0249293a31de359691e868d99527d92cd946
Shiphter <stephantl@gmail.com>
2014-02-18 10:31:16 +0100

    Added jdom.jar and reference + first function for HttpHandler

--- Handin3/work/web/tek/icouldntcareless/musicshop/helpers/HttpHandler.java
+++ Handin3/work/web/tek/icouldntcareless/musicshop/helpers/HttpHandler.java
@@ -1,5 +1,47 @@
 package web.tek.icouldntcareless.musicshop.helpers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.XMLOutputter;
+
 public class HttpHandler {
 
+	public Document outputXMLonHTTP(String httpRequestType, URL url,
+			Document docToOutput) throws ProtocolException, IOException,
+			JDOMException {
+		XMLOutputter outputter = new XMLOutputter();
+
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod(httpRequestType);
+		con.setDoOutput(true);
+		con.setDoInput(true);
+		con.connect();
+
+		outputter.output(docToOutput, con.getOutputStream());
+
+		Document responseDocument = null;
+
+		try {
+			SAXBuilder builder = new SAXBuilder();
+			responseDocument = builder.build((InputStream) con.getContent());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		if (con.getResponseCode() != 200) {
+			System.out.print("An network error occurred: "
+					+ con.getResponseCode() + " - " + con.getResponseMessage());
+		}
+
+		con.disconnect();
+
+		return responseDocument;
+	}
 }
