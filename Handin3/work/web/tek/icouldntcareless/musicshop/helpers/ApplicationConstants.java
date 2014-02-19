054ccdd64968c8f73e62fcd45dea5bd2f39d10a7
Shiphter <stephantl@gmail.com>
2014-02-19 09:17:03 +0100

    Wrong SHOPID

--- Handin3/work/web/tek/icouldntcareless/musicshop/helpers/ApplicationConstants.java
+++ Handin3/work/web/tek/icouldntcareless/musicshop/helpers/ApplicationConstants.java
@@ -2,20 +2,18 @@ package web.tek.icouldntcareless.musicshop.helpers;
 
 import org.jdom2.Namespace;
 
-
 /**
- * @author shiphter
- * Class containing application constants. Important URL's etc.
- * All Constants must be "public static final"
+ * Class containing application constants. Important URL's etc. All Constants
+ * must be "public static final"
  */
 public class ApplicationConstants {
-	public static final Namespace WEBTEKNAMESPACE = Namespace.getNamespace(
-			"w", "http://www.cs.au.dk/dWebTek/2014");
-	public static final String SHOPID = "466";
+	public static final Namespace WEBTEKNAMESPACE = Namespace.getNamespace("w",
+			"http://www.cs.au.dk/dWebTek/2014");
+	public static final String SHOPID = "446";
 	public static final String SHOPKEY = "79D23EFCA0DAAD24E5FFF385";
 	public static final String CLOUDURL = "http://services.brics.dk/java4/cloud";
 	public static final String CREATEITEM = "/createItem";
 	public static final String MODIFYITEM = "/modifyItem";
-	public static final String LISTITEMS = "/listItems?shopID="+SHOPID;
-	
+	public static final String LISTITEMS = "/listItems?shopID=" + SHOPID;
+
 }
