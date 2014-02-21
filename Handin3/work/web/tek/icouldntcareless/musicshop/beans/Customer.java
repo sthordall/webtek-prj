8fb647ae90d4e10421093494a207c9640c4a5683
Shiphter <stephantl@gmail.com>
2014-02-21 16:36:25 +0100

    added isAdmin boolean

--- Handin3/work/web/tek/icouldntcareless/musicshop/beans/Customer.java
+++ Handin3/work/web/tek/icouldntcareless/musicshop/beans/Customer.java
@@ -4,23 +4,24 @@ import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean(name = "Customer", eager = true)
 @SessionScoped
 public class Customer implements Serializable {
-	
+
 	private static final long serialVersionUID = 748705489315002184L;
 	private String customerID;
 	private String customerName;
 	private String customerPass;
-	
+	private Boolean customerIsAdmin;
+
 	@PostConstruct
 	public void init() {
-		customerID="Hans";
+		customerID = "Hans";
+		customerIsAdmin = true;
 	}
-	
+
 	public String getCustomerPass() {
 		return customerPass;
 	}
@@ -32,15 +33,24 @@ public class Customer implements Serializable {
 	public String getCustomerID() {
 		return customerID;
 	}
+
 	public void setCustomerID(String customerID) {
 		this.customerID = customerID;
 	}
+
 	public String getCustomerName() {
 		return customerName;
 	}
+
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
-	
-	
+
+	public Boolean getCustomerIsAdmin() {
+		return customerIsAdmin;
+	}
+
+	public void setCustomerIsAdmin(Boolean customerIsAdmin) {
+		this.customerIsAdmin = customerIsAdmin;
+	}
 }
