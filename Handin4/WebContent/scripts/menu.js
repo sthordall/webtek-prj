0305c8459ba4728d60644ab7ed256de9723dee10
Shiphter <stephantl@gmail.com>
2014-03-07 14:41:08 +0100

    Moved Login linking to menu.js script

--- Handin4/WebContent/scripts/menu.js
+++ Handin4/WebContent/scripts/menu.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$('#products_link').click( function() {
 		$('#page_content').load('views/item_page.html');
 	});
+	
+	$('#login_link').click( function() {
+		$('#page_content').load('views/login.html');
+	});
 });
